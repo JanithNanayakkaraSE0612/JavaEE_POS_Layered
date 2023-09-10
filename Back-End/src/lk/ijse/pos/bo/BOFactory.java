f6e134396361819c0b45c7396eb602f4c859d3c7
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-09-10 17:31:04 +0530

    Complete BO Factory

---
+++ Back-End/src/lk/ijse/pos/bo/BOFactory.java
@@ -0,0 +1,32 @@
+package lk.ijse.pos.bo;
+
+public class BOFactory {
+    private static BOFactory boFactory;
+
+    private BOFactory() {
+
+    }
+
+    public static BOFactory getInstance() {
+        return boFactory == null ? new BOFactory() : boFactory;
+    }
+
+    public enum BOTypes {
+        CUSTOMER, ITEM, ORDER, ORDER_DETAILS
+    }
+
+    public SuperBO getBO(BOTypes types) {
+        switch (types) {
+            case CUSTOMER:
+                return new CustomerBOImpl();
+            case ITEM:
+                return new ItemBOImpl();
+            case ORDER:
+                return new PurchaseOrderBOImpl();
+            case ORDER_DETAILS:
+                return new OrderDetailsBOImpl();
+            default:
+                return null;
+        }
+    }
+}
