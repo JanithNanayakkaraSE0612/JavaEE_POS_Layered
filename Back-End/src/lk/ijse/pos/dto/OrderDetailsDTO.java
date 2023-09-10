e7684443e9ae37b7e83221e095c0be29592dd0ab
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-09-10 15:21:37 +0530

    Complete DTO package

--- Back-End/src/lk/ijse/pos/dto/OrderDetailsDTO.java
+++ Back-End/src/lk/ijse/pos/dto/OrderDetailsDTO.java
@@ -1,4 +1,15 @@
 package lk.ijse.pos.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@ToString
 public class OrderDetailsDTO {
+    private String orderId;
+    private String itemCode;
+    private double price;
+    private int qty;
 }
