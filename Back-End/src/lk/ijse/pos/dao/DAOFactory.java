6003140dfd57bbb5f6928edf8d911f2b0ae3ebff
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-09-10 17:21:54 +0530

    OrderDAO and OrderDAOImpl

--- Back-End/src/lk/ijse/pos/dao/DAOFactory.java
+++ Back-End/src/lk/ijse/pos/dao/DAOFactory.java
@@ -2,6 +2,7 @@ package lk.ijse.pos.dao;
 
 import lk.ijse.pos.dao.custom.impl.CustomerDAOImpl;
 import lk.ijse.pos.dao.custom.impl.ItemDAOImpl;
+import lk.ijse.pos.dao.custom.impl.OrderDAOImpl;
 
 public class DAOFactory {
     private static DAOFactory daoFactory;
