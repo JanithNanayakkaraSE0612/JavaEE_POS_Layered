d878f6049818a74c9eb1efcac914489e6c254483
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-09-17 13:27:40 +0530

    Update Auth Filter

--- Back-End/src/lk/ijse/pos/servlet/filter/AuthFilter.java
+++ Back-End/src/lk/ijse/pos/servlet/filter/AuthFilter.java
@@ -28,16 +28,11 @@ public class AuthFilter implements Filter {
 
         String auth = req.getHeader("Auth");
 
-        //so, if the Auth header is present and username & password
-        //are matching with every request then we can proceed the request
-        //to relevant servlet
         System.out.println(auth);
         if (auth != null && auth.equals("user=admin,pass=admin")) {
-            //forward the request to the requested servlet
             filterChain.doFilter(servletRequest, servletResponse);
         } else {
-            //otherwise send a json object that the clint is not allowed
-            //to access the service
+         
             res.addHeader("Content-Type", "application/json");
             res.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 
